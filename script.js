9951e58cd56078a9c9f014f0658889e7da4ab490
JAYASURYA KADAKUZHILL JOB <161327970+Jayasurya016@users.noreply.github.com>
2025-09-17 18:18:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,83 @@
 //your JS code here. If required.
+
+const title = document.getElementById("title");
+const author = document.getElementById("author");
+const isbn = document.getElementById("isbn");
+const submitBtn = document.getElementById("submit");
+const bookList = document.getElementById("book-list");
+
+submitBtn.addEventListener("click", function(e) 
+{
+	e.preventDefault();
+
+	if(title.value === "" || author.value === "" || isbn.value === "")
+	{
+		alert("Please fill in the fields");
+		return;
+	}
+
+	const row = document.createElement("tr");
+
+	row.innerHTML = `
+	<tr>${title.value}</td>
+	<td>${author.value}</td>
+	<td>${isbn.value}</td>
+	<td><button class="delete">Clear</button></td>`;
+
+	bookList.appendChild(row);
+
+	title.value = "";
+	author.value = "";
+	isbn.value = "";
+})
+
+bookList.addEventListener("click", function (e) 
+{
+	if (e.target.classList.contains("delete")) 
+	{
+		e.target.parentElement.parentElement.remove();
+	}
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
